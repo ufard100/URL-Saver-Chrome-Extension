cbdd382e0a33c3d9f0a7fdd895816290bf0380ad
ufard100 <ufuk.arda@hhu.de>
2022-10-09 14:29:29 +0200

    URL-Saver

---
+++ index.js
@@ -0,0 +1,42 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const urlFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (urlFromLocalStorage) {
+    myLeads = urlFromLocalStorage
+    renderLeads()
+}
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
